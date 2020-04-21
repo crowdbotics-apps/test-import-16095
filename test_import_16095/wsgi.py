3127da6bd779046bc333335597c299fe27cc2dd6
Crowdbotics <team@crowdbotics.com>
2020-04-21 03:49:40 +0000

    Initial commit.

---
+++ test_import_16095/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for test_import_16095 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_import_16095.settings')
+
+application = get_wsgi_application()
